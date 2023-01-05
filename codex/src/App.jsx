33a12842f0748a238a89ba320a6feb7c63153bd4
Subham Sahu <about.subhamsahu@gmail.com>
2023-01-05 14:33:55 +0530

    05012023

--- codex/src/App.jsx
+++ codex/src/App.jsx
@@ -1,39 +1,64 @@
-import React, { useState, useReducer } from "react";
+// Why React doesn't update state immediately
 
-const reducer = (state, action) => {};
-// reducer function takes two parameters - 1. Current State, 2. action
-// this action is we pass into dispatch function
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
-export default function App() {
-  const [state, dispatch] = useReducer(reducer, { count: 0 });
-  // this dispatch function is what we call in order to update our state. Essentially it's going to call our reducer function on line - 3.
+const App = () => {
+  // const [name, setName] = useState("initial");
+  // const [age, setAge] = useState(0);
+  // const [address, setAddress] = useState("initial");
 
-  // Whenever we call dispatch function, its gonna get set to the action variable
+  const [inputText, setInputText] = useState("");
 
-  // const [count, dispatch] = useReducer(reducer, 0);
-  // useReducer() takes two different parameters
-  // - 1. a reducer, which is a function that we perform on our state to get a new state
-  // - 2. initial state value
+  // useEffect(() => {
+  //   console.log(name, age, address);
+  // }, [name, age, address]);
 
-  const [count, setCount] = useState(0);
+  // const handleClick = () => {
+  //   setName("updated");
+  //   console.log(name);
 
-  const decrement = () => {
-    setCount((prevCount) => prevCount - 1);
-  };
+  //   setAge(25);
+  //   console.log(age);
 
-  const increment = () => {
-    setCount((prevCount) => prevCount + 1);
+  //   setAddress("updated");
+  //   console.log(address);
+  // };
+
+  useEffect(() => {
+    if (inputText === "caveat") {
+      console.log("Caveat of useState() get captured");
+    }
+  }, [inputText]);
+
+  const inputChangeHandler = (event) => {
+    setInputText(event.target.value);
+
+    // if (inputText === "caveat") {
+    //   console.log("Caveat of useState() get captured");
+    // }
   };
 
   return (
     <React.Fragment>
       <div>
-        <button onClick={decrement}>-</button>
-
-        <span>{count}</span>
-
-        <button onClick={increment}>+</button>
+        <p>{/* DB: {name} {age} {address} */}</p>
+        {/* <button onClick={handleClick}>Click</button> */}
+      </div>
+      <div>
+        <input value={inputText} onChange={inputChangeHandler} />
       </div>
     </React.Fragment>
   );
-}
+};
+
+export default App;
+
+// first time button click will log (line - 18, 21, 24) the initial values of all state variables. But render all updated values. But log (line - 13) from the useEffect will logs all updated values.
+
+// second time button click will log (line - 18, 21, 24) the updated values of all state variables.
+
+// Why that?
+// =>
+// Because, react checks all the requested updates & completes the updates and then render them at once.
