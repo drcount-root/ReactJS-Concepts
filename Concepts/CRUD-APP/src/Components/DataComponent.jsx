8b68c7dc2d6d15fca9db8f4d2fc047a3ab606465
Subham Sahu <about.subhamsahu@gmail.com>
2023-01-12 23:06:11 +0530

    12012023-22:56

--- Concepts/CRUD-APP/src/Components/DataComponent.jsx
+++ Concepts/CRUD-APP/src/Components/DataComponent.jsx
@@ -1,12 +1,16 @@
 import React, { useEffect } from "react";
+import { useState } from "react";
 
 export default function DataComponent() {
+  const [userdata, setUserData] = useState({ name: "", email: "", dob: "" });
+
   useEffect(() => {
     fetch("https://randomuser.me/api/")
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
         console.log(data.results[0]);
+        
       });
   }, []);
 
