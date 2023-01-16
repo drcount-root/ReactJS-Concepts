1526616a14dfd13b473af4be29c707e29f101f83
Subham Sahu <about.subhamsahu@gmail.com>
2023-01-16 20:03:48 +0530

    16012023

--- codex/src/App.jsx
+++ codex/src/App.jsx
@@ -1,28 +1,30 @@
-import React, { useReducer } from "react";
+import React, { useState, useReducer } from "react";
 
 const ACTIONS = {
-  INCREMENT: "increment",
-  DECREMENT: "decrement",
+  ADD_TODO: "add-todo",
+  TOGGLE_TODO: "toggle-todo",
 };
 
 // reducer function takes two parameters -
 // 1. Current State
 // 2. action
 // this action is we pass into dispatch function
-const reducer = (state, action) => {
+const reducer = (todos, action) => {
   switch (action.type) {
-    case ACTIONS.INCREMENT:
-      return { count: state.count + 1 };
-    case ACTIONS.DECREMENT:
-      return { count: state.count - 1 };
-    default:
-      return state;
+    case ACTIONS.ADD_TODO:
+      return [...todos, newTodo(action.payload.name)];
+    case ACTIONS.TOGGLE_TODO:
+      return [];
   }
 };
 // whenever we call dispatch(), its going to get set to the action variable and then our current state is going to be in the state variable & reducer is going to return our new updated state
 
+const newTodo = (name) => {
+  return { id: Date.now(), name: name, complete: false };
+};
+
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  const [todos, dispatch] = useReducer(reducer, []);
   // useReducer() takes two different parameters
   // - 1. a reducer, which is a function that we perform on our state to get a new state
   // - 2. initial state value
@@ -31,19 +33,41 @@ export default function App() {
   // 1. state
   // 2. dispatch() function, a function which we call in order to update our state. This dispatch() func going to call the reducer() function.
 
-  const increment = function () {
-    dispatch({ type: ACTIONS.INCREMENT });
-  };
+  const [name, setName] = useState("");
 
-  const decrement = function () {
-    dispatch({ type: ACTIONS.DECREMENT });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name } });
+    setName("");
   };
+  // payload is an object which essentially contains all of the variable values we need to perform that action
 
   return (
     <React.Fragment>
-      <button onClick={decrement}>-</button>
-      <span>{state.count}</span>
-      <button onClick={increment}>+</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+      </form>
+
+      {todos.map((todo) => {
+        <Todo key={todo.id} todo={todo} />;
+      })}
     </React.Fragment>
   );
 }
+
+const Todo = ({ todo }) => {
+  return (
+    <div>
+      <span style={{ color: todo.complete ? "#AAA" : "#000" }}>
+        {todo.name}
+      </span>
+
+      <button>Toggle</button>
+      <button>Delete</button>
+    </div>
+  );
+};
