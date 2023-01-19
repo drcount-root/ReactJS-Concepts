2fc3caf38de39f3d059a9c9b26d732af82d182d3
Subham Sahu <about.subhamsahu@gmail.com>
2023-01-19 12:20:34 +0530

    19012023

---
+++ React-With-Redux/1/src/Shop.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Shop() {
+  return (
+    <div>
+      <h2>Nike Jordan</h2>
+      <button className="btn btn-primary mx-2">-</button>
+      Add to cart
+      <button className="btn btn-primary mx-2">+</button>
+    </div>
+  );
+}
