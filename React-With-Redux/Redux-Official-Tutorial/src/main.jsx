0affd8b6b04ba95c6c2997cf307acb7812a8f6f2
Subham Sahu <about.subhamsahu@gmail.com>
2023-01-20 14:18:48 +0530

    20012023

--- React-With-Redux/Redux-Official-Tutorial/src/main.jsx
+++ React-With-Redux/Redux-Official-Tutorial/src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
+// import Provider from 'react-redux' module
+// import store.js
+// wrap the <App/> component with Provider & pass store to the value of store
+// <Provider store={store}>
+//  <App/>
+// </Provider>
