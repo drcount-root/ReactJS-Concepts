b9606ce772a49be7f202a6ea67245ffbff94dbc0
Subham Sahu <about.subhamsahu@gmail.com>
2023-01-21 18:27:27 +0530

    21012023

---
+++ React-With-Redux/React_Redux_Saga/src/redux/productSaga.js
@@ -0,0 +1,7 @@
+// generator function used here to handle async operations
+
+function* productSaga() {
+
+}
+
+export default productSaga;
--- React-With-Redux/React_Redux_Saga/src/redux/store.js
+++ React-With-Redux/React_Redux_Saga/src/redux/store.js
@@ -1,11 +1,20 @@
 // import { createStore } from "redux";
 
 import { configureStore } from "@reduxjs/toolkit";
+import productSaga from "./productSaga";
+import createSagaMiddleware from "redux-saga";
 
 import rootReducer from "./rootReducer";
 
 // const store = createStore(rootReducer);
 
-const store = configureStore({ reducer: rootReducer });
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: () => [sagaMiddleware],
+});
+
+sagaMiddleware.run(productSaga);
 
 export default store;
