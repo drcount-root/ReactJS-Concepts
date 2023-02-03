d76c6a9ec424dffe1f352ba7d6229fb5382bd360
Subham Sahu <about.subhamsahu@gmail.com>
2023-02-03 22:43:06 +0530

    .

--- Concepts/ToDo-CRUD-With-API/src/App.jsx
+++ Concepts/ToDo-CRUD-With-API/src/App.jsx
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
+
+  const [log, setLog] = useState('');
+
+  let r = false;
+
+  useEffect(() => {
+    const fetcher = async () => {
+      const response = await fetch("http://localhost:3000/results");
+      const data = response.data();
+      console.log(data);
+    };
+
+    if (!r) {
+      fetcher().catch((err) => err.message);
+    }
+
+    return () => {
+      r = true;
+    };
+  }, []);
+
   return (
     <React.Fragment>
       <h1>App</h1>
